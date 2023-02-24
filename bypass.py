56cddf2a235baefb0be8775f54db208d75bd6e78
NoSkill <67742372+NoSkill33@users.noreply.github.com>
2023-02-24 09:10:42 +0100

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -3,11 +3,9 @@ import datetime
 from discord.ext import commands
 import requests
 
-##################################################################################################################
 bot      = commands.Bot(command_prefix='o!', description=" ")
 intents  = discord.Intents().all()
 
-##################################################################################################################
 @bot.command(   )
 async def bypass(ctx, arg):
   r=requests.get('https://bypass.bot.nu/bypass2?url='+arg)
@@ -16,17 +14,11 @@ async def bypass(ctx, arg):
   dest = chunks[1]
   stripped = dest.split('"')
   embed = discord.Embed()
-  #embed.set_thumbnail(url="https://cdn.discordapp.com/icons/892738398278787083/b0c1804bf9ec517e5fd89485cb671c02.png?size=4096")
   embed.add_field(name="Bypassed:", value=stripped[3], inline=False)
   await ctx.send(embed=embed)
-  print(f"{ctx.author} used bypass on ({arg}/{stripped[3]})")
-
-##################################################################################################################
 @bot.event
 async def on_ready(  ):
-    await bot.change_presence(activity=discord.Game('Omegus Bypass'))
-    print('                 Logged in as:\n[>] {0.user.name}\n[>] {0.user.id}'.format(bot))
+    await bot.change_presence(activity=discord.Game('https://github.com/NoSkill33'))
+    print('logged in as:\n[>] {0.user.name}\n[>] {0.user.id}'.format(bot))
 
-
-    print('                 Bot Is Ready')
-bot.run("BOT TOKEN")
+bot.run("enter token here")
