a861f011c6258098da6bb74c7c621428b301f20c
NoSkill <67742372+NoSkill33@users.noreply.github.com>
2023-02-26 22:14:00 +0100

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -6,7 +6,7 @@ import requests
 bot      = commands.Bot(command_prefix='o!', description=" ")
 intents  = discord.Intents().all()
 
-@bot.command(   )
+@bot.command()
 async def bypass(ctx, arg):
   r=requests.get('https://bypass.bot.nu/bypass2?url='+arg)
   a = ('%'+r.text)
@@ -17,8 +17,7 @@ async def bypass(ctx, arg):
   embed.add_field(name="Link:", value=stripped[3], inline=False)
   await ctx.send(embed=embed)
 @bot.event
-async def on_ready(  ):
+async def on_ready():
     await bot.change_presence(activity=discord.Game('https://github.com/NoSkill33'))
-    print('logged in as:\n[>] {0.user.name}\n[>] {0.user.id}'.format(bot))
 
 bot.run("enter token here")
